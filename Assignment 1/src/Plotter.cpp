e9d4e61de13a8738890a2920addec555c374e9d3
wangxl <626808285@qq.com>
2023-05-31 00:46:15 +0800

    Assignment 1 finished.

--- Assignment 1/src/Plotter.cpp
+++ Assignment 1/src/Plotter.cpp
@@ -1,7 +1,63 @@
 #include "Plotter.h"
+#include "strlib.h"
 using namespace std;
 
+struct Location {
+    double x;
+    double y;
+};
+
+void moveAbs(Location& p, string& line, const PenStyle& pstyle, const bool& state);
+void moveRel(Location& p, string& line, const PenStyle& pstyle, const bool& state);
+
 void runPlotterScript(istream& input) {
-    /* TODO: Delete this line and the one after it, then implement this function. */
-    (void) input;
+    /* TODO: Delete this line and the one after it, then implement this function.
+     * (void) input;
+     */
+
+    PenStyle pstyle = { 1, "black" };
+    bool state = false;
+    Location p = { 0, 0 };
+
+    for (string line; getline(input, line); ) {
+
+        if (startsWith(toLowerCase(line), "pendown")) {
+            state = true;
+        } else if (startsWith(toLowerCase(line), "penup")) {
+            state = false;
+        } else if (startsWith(toLowerCase(line), "moveabs")) {
+            moveAbs(p, line, pstyle, state);
+        } else if (startsWith(toLowerCase(line), "moverel")) {
+            moveRel(p, line, pstyle, state);
+        } else if (startsWith(toLowerCase(line), "pencolor")) {
+            Vector<string> strList = stringSplit(line, " ");
+            pstyle.color = strList[1];
+
+        } else if (startsWith(toLowerCase(line), "penwidth")) {
+            Vector<string> strList = stringSplit(line, " ");
+            pstyle.width = stringToReal(strList[1]);
+        }
+
+    }
+
+}
+
+void moveAbs(Location& p, string& line, const PenStyle& pstyle, const bool& state) {
+    Vector<string> strList = stringSplit(line, " ");
+    if (state == true) {
+        drawLine(p.x, p.y, stringToReal(strList[1]), stringToReal(strList[2]), pstyle);
+    }
+    p.x = stringToReal(strList[1]);
+    p.y = stringToReal(strList[2]);
+
+}
+
+
+void moveRel(Location& p, string& line, const PenStyle& pstyle, const bool& state) {
+    Vector<string> strList = stringSplit(line, " ");
+    if (state == true) {
+        drawLine(p.x, p.y, p.x+stringToReal(strList[1]), p.y+stringToReal(strList[2]), pstyle);
+    }
+    p.x += stringToReal(strList[1]);
+    p.y += stringToReal(strList[2]);
 }
--- Assignment 1/src/Sandpiles.cpp
+++ Assignment 1/src/Sandpiles.cpp
@@ -7,10 +7,22 @@
 using namespace std;
 
 void dropSandOn(Grid<int>& world, int row, int col) {
-    /* TODO: Delete this line and the three after it, then implement this function. */
-    (void) world;
-    (void) row;
-    (void) col;
+    /* TODO: Delete this line and the three after it, then implement this function.
+     * (void) world;
+     * (void) row;
+     * (void) col;
+     */
+    if (world.inBounds(row, col)) {
+        world[row][col]++;
+        if (world[row][col] >= 4) {
+            world[row][col] = 0;
+            dropSandOn(world, row-1, col);
+            dropSandOn(world, row+1, col);
+            dropSandOn(world, row, col-1);
+            dropSandOn(world, row, col+1);
+        }
+    }
+
 }
 
 
